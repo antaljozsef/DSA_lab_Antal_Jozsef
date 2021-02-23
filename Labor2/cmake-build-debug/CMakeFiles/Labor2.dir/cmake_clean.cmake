fca0d040d91250c60619042b5b99ac60514bf777
Antal József <76700822+antaljozsef@users.noreply.github.com>
2021-02-23 14:58:09 +0200

    Header

---
+++ Labor2/cmake-build-debug/CMakeFiles/Labor2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labor2.dir/function.c.obj"
+  "CMakeFiles/Labor2.dir/main.c.obj"
+  "Labor2.exe"
+  "Labor2.exe.manifest"
+  "Labor2.pdb"
+  "libLabor2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Labor2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/DSA_lab_Antal_Jozsef.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DSA_lab_Antal_Jozsef.
+# This may be replaced when dependencies are built.

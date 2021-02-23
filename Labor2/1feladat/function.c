69b7d4ca4a87e5523ac1b62965f0f020a43505e2
Antal József <76700822+antaljozsef@users.noreply.github.com>
2021-02-23 15:02:57 +0200

    Header

---
+++ Labor2/1feladat/function.c
@@ -0,0 +1,9 @@
+//
+// Created by Antal József on 2021. 02. 23..
+//
+
+#include "function.h"
+
+int sum(int a, int b) {
+    return a+b;
+}
---
+++ Labor2/1feladat/function.h
@@ -0,0 +1,8 @@
+//
+// Created by Antal József on 2021. 02. 23..
+//
+
+#ifndef LABOR2_FUNCTION_H
+#define LABOR2_FUNCTION_H
+int sum(int a, int b);
+#endif //LABOR2_FUNCTION_H
